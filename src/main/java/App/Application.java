352c56e9834173e7d285f398b6f00c78b8ac94ec
Ezdies <bvbclubbvb@gmail.com>
2022-02-23 21:36:40 +0100

    Added basic functionality

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -1,22 +1,27 @@
 package App;
 
 import com.google.gson.Gson;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        get("/quote", (request, response) -> {
+        get("/thymeleaf", (request, response) -> {
             URL url = new URL("https://api.quotable.io/random");
             InputStreamReader reader = new InputStreamReader(url.openStream());
-            Deserializer object = new Gson().fromJson(reader, Deserializer.class);
-            Deserializer.printObjectInfo(object);
-            return Deserializer.stringify(object);
+            Deserializer quote = new Gson().fromJson(reader, Deserializer.class);
+            Deserializer.printObjectInfo(quote);
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("quote", quote);
+            return new ThymeleafTemplateEngine().render(new ModelAndView(attributes, "thymeleaf"));
         });
     }
 }
